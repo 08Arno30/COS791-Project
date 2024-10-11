09e10bc3c142e92b1c3edddbae0800cbddb5f711
JsteReubsSoftware <joostereuben0830@gmail.com>
2024-10-11 22:09:03 +0200

    Removed DeepSort algorithm and used YOLO track method

--- src/object_tracking.py
+++ src/object_tracking.py
@@ -9,79 +9,44 @@ selected_video = video_names[1]
 
 video_path = f'../data/original_video_frames_and_labels/videos/{selected_video}'
 
-# Create output directory
-if not os.path.exists('../data/output_videos'):
-    os.makedirs('../data/output_videos', exist_ok=True)
+# # Create output directory
+# if not os.path.exists('../data/output_videos'):
+#     os.makedirs('../data/output_videos', exist_ok=True)
 
-video_out_path = os.path.join('../data/output_videos', f'{selected_video.split(".")[0]}_out.mp4')
+# video_out_path = os.path.join('../data/output_videos', f'{selected_video.split(".")[0]}_out.mp4')
 
 cap = cv2.VideoCapture(video_path)
 ret, frame = cap.read()
 
-cap_out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc(*'MP4V'), cap.get(cv2.CAP_PROP_FPS),
-                          (frame.shape[1], frame.shape[0]))
+# cap_out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc(*'MP4V'), cap.get(cv2.CAP_PROP_FPS),
+#                           (frame.shape[1], frame.shape[0]))
 
 # Load fine-tuned YOLO model
 model = YOLO("../fine_tuned_model/train/weights/best.pt")
 
-# Initialize DeepSort tracker
-tracker = DeepSort(max_age=0, n_init=5, nms_max_overlap=0.5) # parameters might need to be tuned to optimize the tracker
+cap = cv2.VideoCapture(video_path)
 
-# Set bounding box color for detection
-bbox_color = (0, 255, 0)
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
 
-detection_threshold = 0.7
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
 
-while ret:
-    # Run object detection using YOLO
-    # resize the frame to 640x640
-    frame = cv2.resize(frame, (640, 640))
-    results = model(frame)
-    
-    for result in results:
-        detections = []
-        for bbox in result.boxes.data.tolist():
-            x1, y1, x2, y2, score, class_id = bbox
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
 
-            # convert to integers
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
 
-            # apply detection threshold
-            if score > detection_threshold:
-                detections.append([[x1, y1, x2 - x1, y2 - y1], score, class_id])
-        
-        # Update object tracker
-        tracks = tracker.update_tracks(detections, frame=frame)
-
-        # Draw tracking results
-        for track in tracks:
-            if not track.is_confirmed():
-                continue
-            
-            track_id = track.track_id  # track id assigned by DeepSORT
-            ltrb = track.to_ltrb()  # bounding box in format (left, top, right, bottom)
-            
-            # Ensure the bounding box coordinates are valid integers
-            x1, y1, x2, y2 = map(int, ltrb)
-
-            # Check if the bounding box is within the frame dimensions
-            x1, y1 = max(0, x1), max(0, y1)
-            x2, y2 = min(frame.shape[1], x2), min(frame.shape[0], y2)
-
-            # Draw bounding box
-            cv2.rectangle(frame, (x1, y1), (x2, y2), bbox_color, 2)
-            cv2.putText(frame, f"ID: {track_id}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, bbox_color, 2)
-
-    # Display frame
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
         break
-
-    # Write frame to output video
-    cap_out.write(frame)
-
-    ret, frame = cap.read()
 
 cap.release()
 cv2.destroyAllWindows()
